5a08d272996f32b24804085589919d1e7579989a
JoJoBarthold2 <valentin.j.schmidt@web.de>
2024-10-17 11:47:04 +0200

    more simple fixes for docs

--- create_vtl_corpus/create_corpus.py
+++ create_vtl_corpus/create_corpus.py
@@ -41,7 +41,7 @@ class CreateCorpus:
     as is common with Mozillas Common Voice Corpus using MFA
 
 
-    .. code-block :: bash
+    .. code-block:: bash
         corpus/
         ├── validated.txt          a file where the transripts are stored
         |
